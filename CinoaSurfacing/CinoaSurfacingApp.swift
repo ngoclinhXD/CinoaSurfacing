49e015579638f26806495fc172ba1395e43ad9a3
ngoclinhcutevai <tadashiplayz2000@gmail.com>
2025-11-29 17:12:18 +0700

    Initial Commit

---
+++ CinoaSurfacing/CinoaSurfacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinoaSurfacingApp.swift
+//  CinoaSurfacing
+//
+//  Created by Ngoc Linh on 29/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CinoaSurfacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
